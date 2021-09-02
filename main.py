3b42155f48852732de0d411a9c61864483582c0a
Marlyne Hakizimana <marlhaki12@gmail.com>
2021-09-02 01:34:53 +0000

    updating project with new files

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from flask import Flask
+from flask import Flask,request,render_template
 from flask import jsonify
 from timing import mealrec
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    print("hello")
-    return ("Hello and  Welcome! <br> If you need to check which meal time it is, check the meal path. ")
+def my_form():
+    return render_template('my-from.html')
+#def hello():
+  #  """Return a friendly HTTP greeting."""
+   # print("hello")
+    #return ("Hello and  Welcome! <br> If you need to check which meal time it is, check the meal path. ")
 
-
-@app.route('/meal/<name>')
-def meal(name):
-    print(f"This was placed in the url: new-{name}")
-    hr=int(name[0])
-    period=name[1:]
+    
+#@app.route('/meal/<name>')
+@app.route('/',methods=['POST'])
+def my_form_post():
+    #print(f"This was placed in the url: new-{name}")
+    #name=request.form['text']
+    #hr=int(name[0])
+    hr=int(request.form.get("hour"))
+    period=request.form.get("period")
+    #period=name[1:]
     result=mealrec(hr,period)
     return jsonify(result)
 
